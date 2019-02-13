3b12aef48c56e1c53e2a9cd49f6d23a3331c8d2b
fumikito <guy@hametuha.com>
2019-02-13 20:53:07 +0900

    Add pattern dependencies.

--- src/WPametu/Traits/Reflection.php
+++ src/WPametu/Traits/Reflection.php
@@ -15,6 +15,8 @@ use WPametu\Pattern\Singleton;
 trait Reflection
 {
 
+	use \Hametuha\Pattern\Utility\Reflection;
+
     /**
      * Check if specified class is singleton
      *
@@ -33,32 +35,5 @@ trait Reflection
      */
     protected function is_controller($class_name){
         return $this->is_sub_class_of($class_name, Controller::class);
-    }
-
-    /**
-     * Detect if specifies class is subclass
-     *
-     * @param string $class_name
-     * @param string $should Parent class name
-     * @param bool $allow_abstract Default false
-     * @return bool
-     */
-    protected function is_sub_class_of($class_name, $should, $allow_abstract = false){
-        if( class_exists($class_name) ){
-            // Check if this is subclass
-            $refl = new \ReflectionClass($class_name);
-            return ( $allow_abstract || !$refl->isAbstract() ) && $refl->isSubclassOf($should);
-        }
-        return false;
-    }
-
-    /**
-     * Returns all defined constants
-     *
-     * @return array
-     */
-    public static function get_all_constants(){
-        $refl = new \ReflectionClass(get_called_class());
-        return $refl->getConstants();
     }
 }
